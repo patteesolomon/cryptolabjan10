de87abc1f67d21b71d2354335dd113611d37d77f
patteesolomon <patteesolomon@gmail.com>
2023-01-10 11:17:17 -0600

    Xeno not Mellow

---
+++ App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+//Import route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from "./components/nav";
+
+export default function App () {
+  // We will use the Route component to specify each route
+  return (
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Main/>}/>
+      <Route path="/currencies" element={<Currencies/>}/>
+      <Route path="/price" element={<Price/>}/>
+    </Routes>
+    </div>
+  );
+}
